de0f1f408048b5ca17a9e22eead08003924fda2d
wendeppkdc <wenguanjung@aliyun.com>
2020-10-30 10:26:32 +0800

    Update setup_data.sh

--- setup_data.sh
+++ setup_data.sh
@@ -18,26 +18,6 @@ dlx $conll_url/test conll-2012-test-official.v9.tar.gz
 dlx $conll_url conll-2012-scripts.v3.tar.gz
 dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
 
-bash #!/bin/bash
-
-ontonotes_path=$1
-data_dir=$2
-
-dlx() {
-  wget -P $data_dir $1/$2
-  tar -xvzf $data_dir/$2 -C $data_dir
-  rm $data_dir/$2
-}
-
-conll_url=http://conll.cemantix.org/2012/download
-dlx $conll_url conll-2012-train.v4.tar.gz
-dlx $conll_url conll-2012-development.v4.tar.gz
-dlx $conll_url/test conll-2012-test-key.tar.gz
-dlx $conll_url/test conll-2012-test-official.v9.tar.gz
-
-dlx $conll_url conll-2012-scripts.v3.tar.gz
-dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
-
 bash /content/data/conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data $data_dir/conll-2012
 
 function compile_partition() {
